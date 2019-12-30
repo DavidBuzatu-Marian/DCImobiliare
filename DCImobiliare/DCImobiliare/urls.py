87e336452396048ac8cc64c846f09fa8ca050ffb
davidbuzatu-marian <davidm.buzatu@gmail.com>
2019-12-30 13:23:07 +0200

    Updated requirements.txt

--- DCImobiliare/DCImobiliare/urls.py
+++ DCImobiliare/DCImobiliare/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', include('DCImobiliare.DCImobiliare.frontend.urls')),
-    path('', include('DCImobiliare.DCImobiliare.properties.urls')),
+    path('', include('DCImobiliare.frontend.urls')),
+    path('', include('DCImobiliare.properties.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
