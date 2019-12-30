2abee8a291e77053674dc67d02226057558ec178
davidbuzatu-marian <davidm.buzatu@gmail.com>
2019-12-30 13:31:09 +0200

    Updated requirements.txt

--- DCImobiliare/properties/urls.py
+++ DCImobiliare/properties/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .api import PropertyViewSet
 
 router = routers.DefaultRouter()
-router.register('api/properties', PropertyViewSet, 'properties')
+router.register('api/properties', PropertyViewSet, 'DCImobiliare.properties')
 
 urlpatterns = router.urls
