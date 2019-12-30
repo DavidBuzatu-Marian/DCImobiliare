4d5dfcb762ebb2be2bbb56136bfc339792d9d53d
davidbuzatu-marian <davidm.buzatu@gmail.com>
2019-12-30 13:19:02 +0200

    Updated requirements.txt

--- DCImobiliare/DCImobiliare/settings.py
+++ DCImobiliare/DCImobiliare/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'DCImobiliare.DCImobiliare.urls'
 
 TEMPLATES = [
     {
--- DCImobiliare/DCImobiliare/urls.py
+++ DCImobiliare/DCImobiliare/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', include('frontend.urls')),
-    path('', include('properties.urls')),
+    path('', include('DCImobiliare.Dcimobiliare.frontend.urls')),
+    path('', include('DCImobiliare.Dcimobiliare.properties.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
